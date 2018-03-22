b6c01f96102a42001d5370c7b0367de6f20c486d
sanderjn <32776165+sanderjn@users.noreply.github.com>
2018-03-21 22:31:01 -0500

    Added GA

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 <!doctype html>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-101734464-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-101734464-1');
+	</script>
+
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/style.css">
 
